3f95b27d5135e8c76366c89ad32243d4018e206e
Andreyhuey <andreyhuey777@gmail.com>
2023-04-29 01:21:16 +0100

    dark theme successfully modified

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+
 export default function Nav() {
-  return <></>;
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const HandleThemeSwitch = () => {
+    setTheme(currentTheme === "dark" ? "light" : "dark");
+  };
+
+  // Render null until the theme loads to avoid mismatch errors
+  if (!mounted) return null;
+
+  return (
+    <nav className="py-10 mb-12 flex justify-between">
+      <h1 className="text-xl font-burtons cursor-pointer">CultureX</h1>
+      <ul className="flex items-center">
+        <li>
+          <button onClick={HandleThemeSwitch} className="text-2xl">
+            {currentTheme === "dark" ? (
+              <BsFillSunFill />
+            ) : (
+              <BsFillMoonStarsFill />
+            )}
+          </button>
+        </li>
+        <li>
+          <a
+            className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 cursor-pointer"
+            href="https://shorturl.at/gnI37"
+            target="_blank"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
